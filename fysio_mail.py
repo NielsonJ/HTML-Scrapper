3af278cf141ebbac963b9f3f6773efb32fd07c87
Nielson Janné <nielsonj@gmail.com>
2018-02-04 14:38:33 +0100

    updated readme's

--- fysio_mail.py
+++ fysio_mail.py
@@ -1,4 +1,5 @@
 """
+# HTML-Scrapper
 HTML scraper
 Author: Nielson Janné
 License: MIT
@@ -9,17 +10,17 @@ https://www.defysiotherapeut.com
 Data can be requested through REST API at:
 /restservices/portfolio
 
-note: The REST API returns the first 100 entries regardless of actual query result.
+Known search parameters:
+* postalcode
+* maxdistance (dosn't work propperly within the API)
+* city
+* therapist
+* fysiotype
+* practice
 
-Search parameters REST API accepts:
-    postalcode
-    maxdistance (dosn't work propperly within the API)
-    city
-    therapist
-    fysiotype
-    practice
-
-parameters should be in Dutch language/names
+note: 
+* The REST API returns the first 100 entries regardless of actual query result.
+* parameters should be in Dutch language/names
 """
 import csv
 import json
